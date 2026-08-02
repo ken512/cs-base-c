498c01fa98a086a881dfdd00c662dd2fa5494955
ken512 <ken887265@gmail.com>
2026-08-02 17:57:08 +0900

    ループ、ブール値、while, 抽象化

---
+++ 05_抽象化/test.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+void meow(void);
+
+int main(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    printf("meow\n");
+  }
+}
+
+void meow(void)
+{
+  printf("meow\n");
+}
