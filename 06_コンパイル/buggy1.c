4b58bc9351dbfc8dd22e7ddd3abd04750be92e71
ken512 <ken887265@gmail.com>
2026-08-04 14:28:05 +0900

    メモリや配列

---
+++ 06_コンパイル/buggy1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int get_negative_int(void);
+int main(void)
+{
+  int i = get_negative_int();
+  printf("%i\n", i);
+}
+int get_negative_int(void)
+{
+  int n;
+  do
+  {
+  n = get_int("不の整数: ");
+  } while (n < 0);
+  return n;
+}
---
+++ 07_メモリ/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <cs50.h>
+
+const int TOTAL = 3;
+
+int main(void)
+{
+  int scores[TOTAL];
+  for (int i = 0; i < TOTAL; i++)
+  {
+  scores[i] = get_int("Score: ");
+  }
+
+
+  printf("平均: %f\n",average(TOTAL, scores));
+}
+
+float average(int length, int array[])
+{
+  int sum = 0;
+  for(int i = 0; i < length; i++)
+  {
+    sum += array[i];
+  }
+  return sum / (float) length;
+}
